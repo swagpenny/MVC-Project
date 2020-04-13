8a26ce6f6219de50941068ea427c7d1b2a4b92e1
swagpenny <swagpenny000@gmail.com>
2020-04-12 22:43:15 -0400

    worked more on the api

--- hosted/removeBundle.js
+++ hosted/removeBundle.js
@@ -1,5 +1,23 @@
 //api source: https://rapidapi.com/omgvamp/api/hearthstone?endpoint=5525c4a8e4b01d538895c588
 
+/* testing purpose
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+		"x-rapidapi-key": "6495acf76amshac6bdaf8e37a619p10fdbcjsne8b097b601cf"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+*/
+
+
 var cards;
 var dataPromise;
 
@@ -20,7 +38,7 @@ function getCardData() {
 
 function showCardRandom(){
   var cardNo = Math.floor(Math.random() * cards.length); // Select a random card number
-  showCard(cardNo)
+  showCard(cardNo);
 }
 
 function showCard(cardNo){
